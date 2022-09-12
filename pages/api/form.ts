e33a89619d0423143a9ffe9d57e0a17bc5353e31
Vít Rozsíval <mail@vitrozsival.cz>
2022-09-12 13:42:04 +0200

    chore: add generic to api response

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -1,9 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { sendForm } from '../../services/send-form';
+import { SendFormResponse } from '../../services/types';
 
 const handler = async (
   request: NextApiRequest,
-  response: NextApiResponse,
+  response: NextApiResponse<SendFormResponse>,
 ): Promise<void> => {
   if (request.body) return response.status(200).json(await sendForm(request));
   response.status(404).end();
